809d4555636916483b508dc5d7459a7e93ffe444
Martin Tapia <contact@martintapia.com>
2016-01-12 20:08:20 +0100

    fix OSD border color

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -3,7 +3,7 @@ xscreensaver &
 conky -c ~/.paps/openbox/stats_conkyrc &
 conky -c ~/.paps/openbox/time_conkyrc &
 xsetroot -solid black
-stalonetray --dockapp-mode simple --icon-size=24 --kludges=force_icons_size &
+stalonetray --dockapp-mode simple --icon-size=32 --kludges=force_icons_size -v -bg black -d none &
 ~/.paps/openbox/volume_late.sh &
 
 if [ -f ~/.paps/scripts/local.sh ]
--- openbox/switch.sh
+++ openbox/switch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-eval $(xdotool getmouselocation --shell)
-
 if [ -r ~/.pointer ]
 then
 	eval $(cat ~/.pointer)
