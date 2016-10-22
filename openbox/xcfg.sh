5f376068992ea38d62a2df9aeb12675ba065f296
Martin Tapia <contact@martintapia.com>
2016-10-22 19:08:51 +0200

    rctrl is now a compose key

--- openbox/xcfg.sh
+++ openbox/xcfg.sh
@@ -3,6 +3,7 @@ xset b off
 xset r rate 165 28
 xset m 7/6 6
 setxkbmap -option compose:ralt
+setxkbmap -option compose:rctrl
 setxkbmap -option compose:rwin
 setxkbmap -option compose:menu
 xmodmap ~/.Xmodmap
