34d676ebef08352bebb883b12acda1a9df361cb1
Martin Tapia <contact@martintapia.com>
2017-03-08 19:33:29 +0100

    reflect changes made to dotfiles-server

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # arg number check
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-    echo "Usage: $0 /absolute/path/to/conf/git"
+    echo "Usage: $0 /absolute/path/to/conf/git [--force]"
     exit 1
 fi
 
@@ -22,134 +22,136 @@ case $1 in
         ;;
 esac
 
+rmflags="-ivr"
+if [ "$2" = "--force" ]
+then
+    rmflags="-fvr"
+fi
+
 # dir
 echo "Link: ~/.paps -> $1"
-rm -ivr ~/.paps
+rm $rmflags ~/.paps
 ln -sT "$1" ~/.paps
 
 # X11
 echo "Link: ~/.Xresources -> ~/.paps/x/Xresources"
-rm -ivr ~/.Xresources
+rm $rmflags ~/.Xresources
 ln -sT ~/.paps/x/Xresources ~/.Xresources
 
 echo "Link: ~/.xsession -> ~/.paps/x/xsession"
-rm -ivr ~/.xsession
+rm $rmflags ~/.xsession
 ln -sT ~/.paps/x/xsession ~/.xsession
 
 echo "Link: ~/.xinitrc -> ~/.paps/x/xsession"
-rm -ivr ~/.xinitrc
+rm $rmflags ~/.xinitrc
 ln -sT ~/.paps/x/xsession ~/.xinitrc
 
 echo "Link: ~/.Xmodmap -> ~/.paps/x/Xmodmap"
-rm -ivr ~/.Xmodmap
+rm $rmflags ~/.Xmodmap
 ln -sT ~/.paps/x/Xmodmap ~/.Xmodmap
 
 # pentadactyl
 echo "Link: ~/.pentadactylrc -> ~/.paps/pentadactyl/pentadactylrc"
-rm -ivr ~/.pentadactylrc
+rm $rmflags ~/.pentadactylrc
 ln -sT ~/.paps/pentadactyl/pentadactylrc ~/.pentadactylrc
 
 echo "Link: ~/.pentadactyl -> ~/.paps/pentadactyl/pentadactyl"
-rm -ivr ~/.pentadactyl
+rm $rmflags ~/.pentadactyl
 ln -sT ~/.paps/pentadactyl/pentadactyl ~/.pentadactyl
 
 # icons
 echo "Link: ~/.icons -> ~/.paps/icons"
-rm -ivr ~/.icons
+rm $rmflags ~/.icons
 ln -sT ~/.paps/icons ~/.icons
 
 # fonts
 echo "Link: ~/.fonts.conf -> ~/.paps/fonts/fonts.conf"
-rm -ivr ~/.fonts.conf
+rm $rmflags ~/.fonts.conf
 ln -sT ~/.paps/fonts/fonts.conf ~/.fonts.conf
 
 echo "Link: ~/.fonts -> ~/.paps/fonts"
-rm -ivr ~/.fonts
+rm $rmflags ~/.fonts
 ln -sT ~/.paps/fonts ~/.fonts
 
 # openbox
 mkdir -p ~/.config/openbox
 
 echo "Link: ~/.config/openbox/menu.xml -> ~/.paps/openbox/menu.xml"
-rm -ivr ~/.config/openbox/menu.xml
+rm $rmflags ~/.config/openbox/menu.xml
 ln -sT ~/.paps/openbox/menu.xml ~/.config/openbox/menu.xml
 
 echo "Link: ~/.config/openbox/rc.xml -> ~/.paps/openbox/rc.xml"
-rm -ivr ~/.config/openbox/rc.xml
+rm $rmflags ~/.config/openbox/rc.xml
 ln -sT ~/.paps/openbox/rc.xml ~/.config/openbox/rc.xml
 
 echo "Link: ~/.config/openbox/autostart.sh -> ~/.paps/openbox/autostart.sh"
-rm -ivr ~/.config/openbox/autostart.sh
+rm $rmflags ~/.config/openbox/autostart.sh
 ln -sT ~/.paps/openbox/autostart.sh ~/.config/openbox/autostart.sh
 
 mkdir -p ~/.themes
 
 echo "Link: ~/.themes/modern-grey -> ~/.paps/openbox/modern-grey"
-rm -ivr ~/.themes/modern-grey
+rm $rmflags ~/.themes/modern-grey
 ln -sT ~/.paps/openbox/modern-grey ~/.themes/modern-grey
 
 # gsimplecal
 mkdir -p ~/.config/gsimplecal
 
 echo "Link: ~/.config/gsimplecal/config -> ~/.paps/gsimplecal/config"
-rm -ivr ~/.config/gsimplecal/config
+rm $rmflags ~/.config/gsimplecal/config
 ln -sT ~/.paps/gsimplecal/config ~/.config/gsimplecal/config
 
 # parcellite
 mkdir -p ~/.config/parcellite
 
 echo "Link: ~/.config/parcellite/parcelliterc -> ~/.paps/parcellite/parcelliterc"
-rm -ivr ~/.config/parcellite/parcelliterc
+rm $rmflags ~/.config/parcellite/parcelliterc
 ln -sT ~/.paps/parcellite/parcelliterc ~/.config/parcellite/parcelliterc
 
 # vim
 echo "Link: ~/.vimrc -> ~/.paps/vim/vimrc"
-rm -ivr ~/.vimrc
+rm $rmflags ~/.vimrc
 ln -sT ~/.paps/vim/vimrc ~/.vimrc
 
 echo "Link: ~/.gvimrc -> ~/.paps/vim/gvimrc"
-rm -ivr ~/.gvimrc
+rm $rmflags ~/.gvimrc
 ln -sT ~/.paps/vim/gvimrc ~/.gvimrc
 
 echo "Link: ~/.vim -> ~/.paps/vim/vim"
-rm -ivr ~/.vim
+rm $rmflags ~/.vim
 ln -sT ~/.paps/vim/vim ~/.vim
 
 # nvim
 echo "Link: ~/.config/nvim -> ~/.paps/vim/vim"
-rm -ivr ~/.config/nvim
+rm $rmflags ~/.config/nvim
 ln -sT ~/.paps/vim/vim ~/.config/nvim
-
-echo "Link: ~/.config/nvim/init.vim -> ~/.paps/vim/vimrc"
-rm -ivr ~/.config/nvim/init.vim
-ln -sT ~/.paps/vim/vimrc ~/.config/nvim/init.vim
 
 # zsh
 echo "Link: ~/.zshrc -> ~/.paps/zsh/zshrc"
-rm -ivr ~/.zshrc
+rm $rmflags ~/.zshrc
 ln -sT ~/.paps/zsh/zshrc ~/.zshrc
 
 echo "Link: ~/.zshenv -> ~/.paps/zsh/zshenv"
-rm -ivr ~/.zshenv
+rm $rmflags ~/.zshenv
 ln -sT ~/.paps/zsh/zshenv ~/.zshenv
 
 echo "Link: ~/.inputrc -> ~/.paps/zsh/inputrc"
-rm -ivr ~/.inputrc
+rm $rmflags ~/.inputrc
 ln -sT ~/.paps/zsh/inputrc ~/.inputrc
 
 # git
 echo "Link: ~/.gitconfig -> ~/.paps/git/gitconfig"
-rm -ivr ~/.gitconfig
+rm $rmflags ~/.gitconfig
 ln -sT ~/.paps/git/gitconfig ~/.gitconfig
 
 # tmux
 echo "Link: ~/.tmux.conf -> ~/.paps/tmux/tmux.conf"
-rm -ivr ~/.tmux.conf
+rm $rmflags ~/.tmux.conf
 ln -sT ~/.paps/tmux/tmux.conf ~/.tmux.conf
 
 # htop
 mkdir -p ~/.config/htop
 
 echo "Link: ~/.config/htop/htoprc -> ~/.paps/htop/htoprc"
-rm -ivr ~/.config/htop/htoprc
+rm $rmflags ~/.config/htop/htoprc
 ln -sT ~/.paps/htop/htoprc ~/.config/htop/htoprc
