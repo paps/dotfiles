dd3481ed64e93fbe2fbf4e0184f78a8aa15446e5
Martin Tapia <contact@martintapia.com>
2018-09-05 12:21:11 +0200

    firefox config

---
+++ firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("nglayout.enable_drag_images", false);
+user_pref("general.smoothScroll", false);
+user_pref("mousewheel.default.delta_multiplier_y", 200);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
