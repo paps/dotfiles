28c6117cbb3fd362079d76e317c55bcb1fcd47f9
Martin Tapia <martin@phantombuster.com>
2019-11-06 00:19:25 +0100

    firefox: do not modify scroll speed now that kinetic scrolling landed

--- firefox/user.js
+++ firefox/user.js
@@ -1,6 +1,6 @@
 user_pref("nglayout.enable_drag_images", false);
 user_pref("general.smoothScroll", false);
-user_pref("mousewheel.default.delta_multiplier_y", 200);
+// user_pref("mousewheel.default.delta_multiplier_y", 200);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("extensions.pocket.enabled", false);
