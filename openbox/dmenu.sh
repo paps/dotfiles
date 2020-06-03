ad3a003dca2a39cd26c685c9cfafd8959006439c
Martin Tapia <martin@phantombuster.com>
2020-06-04 01:07:48 +0200

    using rofi instead of dmenu

--- openbox/dmenu.sh
+++ openbox/dmenu.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec $(dmenu_run -i -nb \#859900 -nf \#fdf6e3 -sb \#d33682 -sf \#fdf6e3 -fn Terminus:bold:pixelsize=32)
+#exec $(dmenu_run -i -nb \#859900 -nf \#fdf6e3 -sb \#d33682 -sf \#fdf6e3 -fn Terminus:bold:pixelsize=32)
+exec $(rofi -modi drun -show drun)
