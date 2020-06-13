de7631ba14a7306228bd66587cebd641c6336d71
Martin Tapia <martin@phantombuster.com>
2020-06-13 23:58:21 +0200

    rwin discord ptt key, so not compose key

--- openbox/xcfg.sh
+++ openbox/xcfg.sh
@@ -1,10 +1,14 @@
 setxkbmap us
+
 xset b off
 xset r rate 165 28
 xset m 7/6 6
+
 setxkbmap -option compose:ralt
 setxkbmap -option compose:rctrl
-setxkbmap -option compose:rwin
 setxkbmap -option compose:menu
+# Not using rwin as a possible compose key because it's a good Discord PTT key
+#setxkbmap -option compose:rwin
+
 xmodmap ~/.Xmodmap
 numlockx on
