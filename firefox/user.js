7e71e61e78c7a0470f51c77fa0929dccf1bf026d
Martin Tapia <martin@phantombuster.com>
2020-06-20 16:49:16 +0200

    black cursor theme

--- firefox/user.js
+++ firefox/user.js
@@ -8,3 +8,4 @@ user_pref("services.sync.addons.ignoreUserEnabledChanges", true); // don't sync 
 user_pref("toolkit.cosmeticAnimations.enabled", false);
 user_pref("ui.caretBlinkTime", 0);
 user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.display.background_color", "#155968"); // solarized dark background
