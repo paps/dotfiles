674a6c19f71a59d0f7171a1f673177fec7b40f12
Martin Tapia <martin@phantombuster.com>
2024-01-12 16:01:29 +0100

    Better Ctrl-Alt-1 paste date shortcut

--- openbox/c-a-1-write-time-and-date.sh
+++ openbox/c-a-1-write-time-and-date.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-xdotool type --clearmodifiers "$(date +'%a, %b %d, %Y, %H:%M')"
+
+# Doing this xclip trick instead of directly writing the date with `xdotool type`
+# because xdotool cannot type any key that is being held down while it runs, such
+# as '1' in the Ctrl-Alt-1 binding used to run this script, which is super annoying.
+
+echo "$(date +'%a, %b %d, %Y, %H:%M')" | xclip -selection clipboard
+xdotool key --clearmodifiers "ctrl+v"
