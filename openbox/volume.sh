2e17dfd5bc9f2fa81e81594ac711bc1fff265e75
Martin Tapia <martin@phantombuster.com>
2024-06-08 16:12:17 +0200

    Use pactl instead of amixer for controlling audio

--- openbox/volume.sh
+++ openbox/volume.sh
@@ -15,19 +15,13 @@ if [ $# -eq 1 ]
 then
     if [ $1 = "+" ]
     then
-        pactl set-sink-volume 0 +5%
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
     elif [ $1 = "-" ]
     then
-        pactl set-sink-volume 0 -5%
+        pactl set-sink-volume @DEFAULT_SINK@ -5%
     elif [ $1 = "m" ]
     then
-        amixer -q set Master toggle
-        amixer -q set Headphone toggle
-        amixer -q set Front toggle
-        amixer -q set Surround toggle
-        amixer -q set Center toggle
-        amixer -q set LFE toggle
-        amixer -q set Line toggle
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
     else
         echo "Usage: $0 +|-|m"
     fi
