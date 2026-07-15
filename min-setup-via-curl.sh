bcd4bfee091395c2b901447d9ddb33f79db77493
Martin <contact@martintapia.com>
2026-07-15 12:19:28 +0800

    Add gh utility

--- min-setup-via-curl.sh
+++ min-setup-via-curl.sh
@@ -17,7 +17,7 @@ sudo apt-get update
 echo ""
 echo "==> Installing packages..."
 echo "//////////////////////////"
-sudo apt-get install -y zsh htop neovim git ripgrep tree tmux mosh file
+sudo apt-get install -y zsh htop neovim git ripgrep tree tmux mosh file gh
 
 echo ""
 echo "==> Downloading ~/.tmux.conf..."
